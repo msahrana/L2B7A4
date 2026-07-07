ef0c74275d09f5e12bf02799560042b4ba508041
Sayed Anower Hossain Rana <msahrana@gmail.com>
2026-07-08 03:58:47 +0600

    prpperty sectio is updated

--- src/modules/properties/property.route.ts
+++ src/modules/properties/property.route.ts
@@ -1,7 +1,7 @@
 import { propertyController } from './property.controller';
-import express from 'express';
+import { Router } from 'express';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/', propertyController.getAllProperties);
 router.get('/:id', propertyController.getSingleProperty);
