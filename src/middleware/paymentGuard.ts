2177327e259cd5d7aebba053d18ac913c02b5fbd
Sayed Anower Hossain Rana <msahrana@gmail.com>
2026-07-08 11:09:45 +0600

    payment guard middleware is added

---
+++ src/middleware/paymentGuard.ts
@@ -0,0 +1,32 @@
+import { PaymentStatus } from '../../generated/prisma/enums';
+import { NextFunction, Request, Response } from 'express';
+import { catchAsync } from '../utils/catchAsync';
+import { prisma } from '../lib/prisma';
+
+export const subscriptionGuard = () => {
+    return catchAsync(
+        async (req: Request, res: Response, next: NextFunction) => {
+            const userId = req.user?.id;
+
+            const payment = await prisma.payment.findUnique({
+                where: {
+                    id: userId,
+                },
+            });
+
+            if (!payment) {
+                throw new Error(
+                    'Please subscribe to get access to Premium Contents',
+                );
+            }
+
+            if (payment?.status !== PaymentStatus.PENDING) {
+                throw new Error(
+                    'Please subscribe again to get access to Premium Contents',
+                );
+            }
+
+            next();
+        },
+    );
+};
